7a579e4960c5367a19515d5ed044178de65344dc
Neo <logicfeel@nate.com>
2020-09-26 14:15:19 +0900

    __inteface 로 수정

--- tests/tasks-i-object.js
+++ tests/tasks-i-object.js
@@ -7,16 +7,15 @@ var util = require("../src/utils");
 var Animal  = (function () {
     function Animal() {
         
-        this._implements(IObject);
+        this._implements(IObject);      // 인터페이스 구현함
     }
 
     Animal.prototype.move  = function(p_name) {
     };
 
     Animal.prototype.getGUID  = function() {
-        // return this.__implements[0].prototype.getGUID.call(this);
-        return this.__implements["IObject"].prototype.getGUID.call(this);
-
+        // return this.__interface[0].prototype.getGUID.call(this);
+        return this.__interface["IObject"].prototype.getGUID.call(this);
     };
 
     return Animal;
@@ -24,6 +23,7 @@ var Animal  = (function () {
 
 
 var a = new Animal();
+
 console.log("getGUID() " + a.getGUID());
 
 console.log("-End-");
