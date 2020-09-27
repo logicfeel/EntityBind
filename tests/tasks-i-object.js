83e75cf0f40ff472ba3dfad5d861347b39bcb83d
Neo <logicfeel@nate.com>
2020-09-27 22:17:27 +0900

    수정했어요

--- tests/tasks-i-object.js
+++ tests/tasks-i-object.js
@@ -4,10 +4,23 @@ var IObject = require("../src/i-object");
 var util = require("../src/utils");
 
 
+
+//=================================
+function IDog () {
+
+}
+IDog.prototype.setSound = function() {
+    return "Set";
+};
+IDog.prototype.getSound = function() {
+    return "Set";
+};
+
+
 var Animal  = (function () {
     function Animal() {
         
-        this._implements(IObject);      // 인터페이스 구현함
+        this._implements(IObject, IDog);      // 인터페이스 구현함
     }
 
     Animal.prototype.move  = function(p_name) {
@@ -24,6 +37,6 @@ var Animal  = (function () {
 
 var a = new Animal();
 
-console.log("getGUID() " + a.getGUID());
+// console.log("getGUID() " + a.getGUID());
 
 console.log("-End-");
