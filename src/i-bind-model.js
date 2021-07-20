1cc654c758e19e8c3a04cfa4f0f5dbb16bcc3fbc
Logic <logicfeel@nate.com>
2021-07-20 21:19:56 +0900

    ...

--- src/i-bind-model.js
+++ src/i-bind-model.js
@@ -56,7 +56,7 @@
              * @property {Function} isNotNull
              * @property {Function} isNullPass
              * @property {Function} callback
-             * @property {Array | Function} constraints 제약조건
+             * @property {Array<IConstraint | Function>} constraints 제약조건
              * @property {Array} constraints.regex 제약조건
              * @property {Array} constraints.msg 제약조건
              * @property {String} constraints.code 실패시 리턴 코드
@@ -80,9 +80,35 @@
              *      faq_idx:        { read:     "bind",     delete:     "bind" }
              * };
              */
+
+
             /**
              * 속성(아이템)
              * @member
+             * @type {Object.<String, IItem | String | Boolean | Number>}
+             * @property {Number} size 크기
+             * @property {String} type 타입
+             * @property {String} default
+             * @property {String} caption
+             * @property {Boolean} isNotNull
+             * @property {Boolean} isNullPass
+             * @property {Function} callback
+             * @property {Array | Function} constraints 제약조건
+             * @property {Array} constraints.regex 제약조건
+             * @property {Array} constraints.msg 제약조건
+             * @property {String} constraints.code 실패시 리턴 코드
+             * @property {Number} constraints.return 성공 조건
+             * @example
+             * this.prop = {
+             *   // inner
+             *   __isGetLoad:    true,
+             *   __listUrl:      "",
+             *   keyword:        { selector: { key: "#m-keyword"+ _SUFF,         type: "value" } },
+             *   page_size:      {
+             *       setter: function(val) { page.page_size = val; },
+             *       selector: { key: "select[name=m-page_size]"+ _SUFF,         type: "value" },
+             *   },
+             * };
              */
             this.prop       = {};
             
@@ -90,6 +116,26 @@
              * 명령
              * @member
              * @type {Object.<String, IBindcommand>}
+             * @property {Number} outputOption 출력방식 0 ~ 3
+             * @property {Function} cbVaild 검사 콜백
+             * @property {Function} cbBind 바인드 전 콜백
+             * @property {Function} cbResult 결과 수신 콜백
+             * @property {Function} cbOutput 출력 콜백
+             * @property {Function} cbEnd 종료 콜백
+             * @property {Function} onExecute 실행전 이벤트
+             * @property {Function} onExecuted 실행후 이벤트
+             * @example
+             * this.command = {
+             *   create:         {
+             *     onExecute: function(p_bindCommand) { 
+             *        _this.bindModel.items["cmd"].value = "CREATE"; 
+             *     },
+             *     cbEnd: function(p_entity) {
+             *         if (p_entity["return"] < 0) return alert("등록 처리가 실패 하였습니다. Code : " + p_entity["return"]);
+             *         location.href = _this.bindModel.prop["__listUrl"];
+             *     },
+             *   },
+             * };
              */
              this.command       = {};
 
@@ -99,13 +145,13 @@
              * @type {Object.<String, Function>}
              * @example
              * this.fn = {
-             * searchList: function() {
-             *      page.page_count = 1;
-             *      _this.bindModel.list.execute();
-             *  },
-             * procList: function () { 
-             *       _this.bindModel.list.execute(); 
-             *  }
+             *   searchList: function() {
+             *     page.page_count = 1;
+             *     _this.bindModel.list.execute();
+             *   },
+             *   procList: function () { 
+             *     _this.bindModel.list.execute(); 
+             *   }
              * };
              */
             this.fn         = {};
@@ -113,35 +159,33 @@
             /** 
              * 속성명(prop)에 매핑을 설정한다.
              * @member  
-             * @type {Object.<String, Object>}  
-             * @property {String} prop 매핑할 속성이름
-             * @property {String} prop.command 매핑할 명령
-             * @property {'Arrary'} prop.command 전체 명령
-             * @property {Object} prop.command.mapping 매핑대상
-             * @property {'valid'} {prop.command.mapping} 검사(엔티티)에 매핑 한다.
-             * @property {'bind'} {prop.command.mapping} 바인딩(엔티티)에 매핑한다.
-             * @property {'output'} {prop.command.mapping} 출력(엔티티)에 매핑한다.
-             * @property {Arrary<'valid', 'output'>} {prop.command.mapping} 검사와 출력에 매핑한다.
-             * @property {'Arrary' | '[]'} {prop.command.mapping} 모든곳에 매핑한다.(검사와 바인딩 출력)
+             * @type {Object.<String, IMapping>}  
+             * @property {String} command 매핑할 명령
+             * @property {'Arrary'} command 전체 명령
+             * @property {'valid'} command.mapping 검사(엔티티)에 매핑 한다.
+             * @property {'bind'} command.mapping 바인딩(엔티티)에 매핑한다.
+             * @property {'output'} command.mapping 출력(엔티티)에 매핑한다.
+             * @property {'[]'} command.mapping 모든곳에 매핑한다.(검사와 바인딩 출력)
              * @example
              * this.mapping = {
              *      cmd:            { Array:    "bind" },
-             *      keyword:        { list:     "bind" },
+             *      keyword:        { list:     ["bind", "valid"] },
              *      faq_idx:        { read:     "bind",     delete:     "bind" }
              * };
-
              */
             this.mapping    = {};
 
             /**
              * 기본 검사 콜백
              * @member
+             * @type {Function}
              */
             this.cbBaseValid    = null;
             
             /**
              * 기본 바인드 콜백
              * @member
+             * @type {Function}
              */
             this.cbBaseBind     = null;
             
@@ -155,51 +199,57 @@
             /**
              * 기본 출력 콜백
              * @member
+             * @type {Function}
              */
             this.cbBaseOutput   = null;
             
             /**
              * 기본 종료 콜백
              * @member
+             * @type {Function}
              */
             this.cbBaseEnd      = null;
 
             /**
              * 실행전 이벤트
              * @member
+             * @type {Function}
              */
             this.onExecute  = null;
             
             /**
              * 실행후 이벤트
              * @member
+             * @type {Function}
              */
             this.onExecuted = null;
 
             /**
              * 실패 콜백
              * @member
+             * @type {Function}
              */
             this.cbFail = function() {};
             
             /**
              * 에러 콜백
              * @member
+             * @type {Function}
              */
             this.cbError = function() {};
         }
         /**
-         * 사전 등록
+         * 초기화시점에(init) 등록
          */
         IBindModel.prototype.preRegister = function() {};
         
         /**
-         * 사전 검사
+         * 초기화시점에(init) 검사
          */
         IBindModel.prototype.preCheck = function() { return true };
 
         /**
-         * 사전 준비
+         * 초기화시점에(init) 준비
          */
         IBindModel.prototype.preReady = function() {};
 
