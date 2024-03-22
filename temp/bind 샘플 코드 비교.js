575d19c63e9c31414e6012b2a52eabf5b560b499
logic <logicfeel@gmail.com>
2024-03-22 13:00:46 +0900

    bind 샘플 소스 작성

--- temp/bind 샘플 코드 비교.js
+++ temp/bind 샘플 코드 비교.js
@@ -1,83 +1,85 @@
-
-/**
- * 서비스 객체를 사용한 경우
- */
-class NewSevice{
-    command = {
-        create: {
-            onExecute(p_bindModel, p_bindCommand) { 
-                p_bindModel.columns['cmd'].value = 'LISt';
-                p_bindModel.fn.moveList();  // 개선함
-            }
-        }
-    }
-    fn = {
-        procDisplayList: function () { 
-            _this.bindModel.command.list_display.execute(); 
-            _this.bm.command.list_display.execute(); 
-        }
-    }
-}
-
 // 기본 설정
 var board = new BindModelAjax();
+
 board.baseUrl = '../Boad.C.asp';
+board.addTable('second');                                       // REVIEW: 공통
+// ##############################
+// 컬럼 및 커맨드 설정
+    // 1. 아이템 추가 후 커맨드에 매핑 설정
+    board.items.add('phone' , '');
+    board.items.add('tel' , '010');
+    board.addCommand('read', 3);
+    board.setMapping({  // 매핑 설정
+        "phone": {'read': ['bind']},
+        "tel": {'read': ['valid', 'bind']},
+        "second.tel": {'read': ['output']},                         // REVIEW:
+    });
+    // 2. 컬럼 추가 및 커맨드에 동시 등록 (아이템 생략)
+    board.addCommand('read', 3);
+    board.addColumn(new HTMLColumn('phone'), 'read', ['bind']);     // POINT: read 자동 등록
+    board.addColumnValue('tel', '010', 'read', ['valid', 'bind']);
+    board.addColumnValue('second.tel', '010', 'read', ['output']);  // REVIEW:
+    // 3. 컬렉션에 컬럼 추가 >> 커맨드 추가 >> 컬럼 설정
+    board.addCommand('read', 3);
+    board.colums.add('phone');
+    board.columns.addValue('tel', '');
+    board._tables['sceond'].columns.addValue('tel', '');
+    board.command['read'].setColumn('phone', 'bind')
+    board.command['read'].setColumn('tel', ['valid', 'bind'])
+    board.command['read'].setColumn('second.tel', ['output'])       // REVIEW:
+    // 4. 컬럼객체 직접 추가
+    board.addCommand('read', 3);
+    board.colums.add('phone');
+    board.columns.addValue('tel', '');
+    board._tables['sceond'].columns.addValue('tel', '');
+    board.command['read'].setColumn('phone', 'bind')
+    board.command['read'].setColumn('tel', ['valid', 'bind'])
+    board.command['read'].addColumn('tel', ['output'])              // REVIEW:
+    // board.command['read'].addColumn(board._tables['sceond'].columns['tel'], ['output'])  // 위와 동일 처리
+    // 5. baseTable
+    board.addCommand('read', 3, 'second');  // board.addCommand('read', 3, board._tables['second']);
+    board.colums.add('phone');
+    board.columns.addValue('tel', '');
+    board._tables['sceond'].columns.addValue('tel', '');
+    board.command['read'].setColumn('phone', 'bind')
+    board.command['read'].setColumn('tel', ['valid', 'bind'])
+    board.command['read'].addColumn('tel', ['output'])              // REVIEW:
+    // board.command['read'].addColumn('second.tel', ['output'])    // 위와 동일 처리
 
-// ###############################
-// 1. 아이템 추가 후 커맨드에 매핑 설정
-board.items.add('phone' , '');
-board.items.add('tel' , '010');
-board.setMapping({  // 매핑 설정
-    phone: {'read': ['bind']},
-    tel: {'read': ['valid', 'bind']}
-});
-// 2. 컬럼 추가 및 커맨드에 동시 등록 (아이템 생략)
-board.addColumn(new HTMLColumn('phone'), 'read', ['bind']); // POINT: read 자동 등록됨
-board.addColumnValue('tel', '010', 'read', ['valid', 'bind']);
-// 3. 컬렉션에 컬럼 추가 >> 커맨드 추가 >> 컬럼 설정
-board.colums.add('phone');
-board.columns.addValue('tel', '');
-board.addCommand('read');
-board.command['read'].setColumn('phone', 'bind')
-board.command['read'].setColumn('tel', ['valid', 'bind'])
-
-// ###############################
-// 커맨드별 콜백 설정
+// ##############################
+// 커맨드별 콜백 및 사용자 함수 등록
 board.command['read'].onExecute = ()=>{};
-board.cmd['read'].onExecuted = ()=>{}; // alias 을 통한 접
+board.cmd['read'].cbBind = ()=>{}; // alias 을 통한 접
+board.fn.add('search', ()=>{});
 
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 // 서비스 객체를 통한 설정
 var board = new BindModelAjax({
     baseUrl: '../Boad.C.asp',       // 객체 속성 설정
+    tables: ['second'],
     items: {
-        keyword: ''
+        phone: '',
+        tel: '010',
     },
-
-    command: {
+    command: {  // cmd
         read: {
-           outputOption: 3,
-           columns: {
-                name: 'tel', 
-                value: '010..',
-                entity: ['vaild', 'bind']
-           },
-           onExecute: function(p_bindCommand) { bm.items['cmd'].value = 'READ'; };
+                        
+            outputOption: 3, // outOpt
+            onExecute: ()=>{},
+            cbBind: ()=>{},
         }
     },
-    prop: { 
-        __pageSize: 10, // 내부용
-        keyword: '',
-        sort_cd: '',
-    },
     mapping: {
-        keyword: { Array: 'bind' }
-        sord_cd: { list: ['valid', 'bind'] }
-    },    
-    preRegister: function(p_this) { // 전처리
-        /* 내용 */
+        phone: { read: 'bind' },
+        tel: { read: ['valid', 'bind'] },
+        "second.tel": { read: 'output' },
     },
+    fn: {
+        search: ()=>{}
+    }
 });
 
+// ##############################
 // 준비 및 실행
 $(function () {
     board.init();           // 초기화
